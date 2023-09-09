d62d95833f3e4cf84e662ddea2a3af43fd0a7ad5
Sam M <snv.macaraniag@gmail.com>
2023-09-08 20:06:58 -0700

    player script

--- Assets/Player/demo_player.gd
+++ Assets/Player/demo_player.gd
@@ -1,14 +1,13 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -800.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -25,10 +24,11 @@ func _physics_process(delta):
 		$AnimSprite.flip_h = false
 		$AnimSprite.play("walk")
 		if direction < 0: $AnimSprite.flip_h = true
-		if not is_on_floor():
-			$AnimSprite.play("jump")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		$AnimSprite.play("idle")
+	
+	if not is_on_floor():
+		$AnimSprite.play("jump")
 
 	move_and_slide()
