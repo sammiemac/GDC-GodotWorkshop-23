62eb00b6c595e66285451c5f75d2d8674069e9ac
Sam M <56046438+sammiemac@users.noreply.github.com>
2023-09-11 19:46:42 -0700

    enemy styuff

--- Assets/Enemies/demo_enemy.gd
+++ Assets/Enemies/demo_enemy.gd
@@ -1,14 +1,29 @@
 extends CharacterBody2D
 
-const SPEED = 50.0
+@export var speed = 100.0
+@export var direction = true
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	
-	velocity.x += SPEED
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	$AnimSprite.play("move")
+	
+	if direction:
+		velocity.x = speed * -1
+		$AnimSprite.flip_h = false
+	else:
+		velocity.x = speed
+		$AnimSprite.flip_h = true
+
 	move_and_slide()
+
+func _on_hit_box_body_entered(body):
+	if body.is_in_group("player"):
+		print("hit!")
